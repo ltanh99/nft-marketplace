900404e0f9c521330375402fa8d79b03a02bbffc
ltanh99 <abcxyz030499@gmail.com>
2023-04-27 22:38:24 +0700

    build nft

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,6 +1,8 @@
+var BoredPetsNFT = artifacts.require("BoredPetsNFT");
+var Marketplace = artifacts.require("Marketplace");
 
-var SimpleStorage = artifacts.require("SimpleStorage");
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-};
+module.exports = async function(deployer) {
+  await deployer.deploy(Marketplace);
+  const marketplace = await Marketplace.deployed();
+  await deployer.deploy(BoredPetsNFT, marketplace.address);
+}
